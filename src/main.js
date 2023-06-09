10fde34430d5dec23f5f2d7b35a976fd6136f7c8
daw2-sorroche23 <sorrocheperezzeus@fpllefia.com>
2023-06-09 15:02:47 +0200

    Examen

---
+++ src/main.js
@@ -0,0 +1,11 @@
+console.log('hola');
+
+import { series } from "./src/datos/series";
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+console.log(series);
